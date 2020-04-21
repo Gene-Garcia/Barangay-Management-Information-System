0eba651505520d153ee8d38c414b91cbb6af8abf
gn-grc <genejogarcia.gg@gmail.com>
2020-04-21 16:07:26 +0800

    login ui

--- Barangay Management Information System/Barangay Management Information System/Controllers/AccountController.cs
+++ Barangay Management Information System/Barangay Management Information System/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace Barangay_Management_Information_System.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
