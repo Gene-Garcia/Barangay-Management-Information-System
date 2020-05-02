a8d1d286afbdbd4e431e0c2f02f4f3fff5830ccb
gn-grc <genejogarcia.gg@gmail.com>
2020-05-02 16:45:48 +0800

    prototype for adding councilors

---
+++ Barangay Management Information System/Barangay Management Information System/Controllers/BarangayOfficialsController.cs
@@ -0,0 +1,35 @@
+﻿using Barangay_Management_Information_System.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barangay_Management_Information_System.Controllers
+{
+    public class BarangayOfficialsController : Controller
+    {
+
+        private DBEntities entities = new DBEntities();
+
+        // GET: BarangayOfficials
+        public ActionResult Index()
+        {
+            return View(entities.ResidentsInformations.ToList());
+        }
+
+        public ActionResult View(string[] residentIds)
+        {
+
+            List<string> ids = new List<string>();
+
+            for (int i = 0; i < residentIds.Length; i++)
+            {
+                ids.Add(residentIds[i]);
+            }
+
+            TempData["Ids"] = ids;
+            return View();
+        }
+    }
+}
