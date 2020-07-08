f63988d82838f23919c72909c0d8e9be0301d459
gn-grc <genejogarcia.gg@gmail.com>
2020-07-08 18:04:59 +0800

    implemented-view, update, and add resident and information

--- Barangay Management Information System/Barangay Management Information System/Controllers/ResidentsController.cs
+++ Barangay Management Information System/Barangay Management Information System/Controllers/ResidentsController.cs
@@ -1,9 +1,11 @@
-﻿using Barangay_Management_Information_System.Models.Entity;
+﻿using Barangay_Management_Information_System.Models;
+using Barangay_Management_Information_System.Models.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
 
 namespace Barangay_Management_Information_System.Controllers
 {
@@ -11,7 +13,7 @@ namespace Barangay_Management_Information_System.Controllers
     {
         private DBEntities entities = new DBEntities();
 
-        // GET: Residents
+        [Authorize]
         public ActionResult Index(string deceased)
         {
 
@@ -39,6 +41,107 @@ namespace Barangay_Management_Information_System.Controllers
             }
 
             return Content(deceased);
+        }
+
+        [Authorize]
+        public ActionResult ViewResident(string residentID)
+        {
+            try
+            {
+                if (residentID == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                ResidentsInformation resident = entities.ResidentsInformations.Where(m => m.ResidentId == residentID).FirstOrDefault();
+                
+                if (resident == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return PartialView("_ViewResident", resident);
+            }
+            catch (Exception e)
+            {
+                TempData["alert-type"] = "alert-danger";
+                TempData["alert-header"] = "Error";
+                TempData["alert-msg"] = "Something went wrong, please try again later.";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult AddUpdateResident(string residentID)
+        {
+            try
+            {
+
+                TempData["Sites"] = entities.Sites.ToList();
+
+                // add
+                if (residentID == null || residentID == "")
+                {
+                    return PartialView("_AddUpdateResident", new RegisterResidentViewModel());
+                }
+                else // update
+                {
+
+                    ResidentsInformation resident = entities.ResidentsInformations.Where(m => m.ResidentId == residentID).FirstOrDefault();
+
+                    RegisterResidentViewModel viewModel = new RegisterResidentViewModel();
+
+                    viewModel.ResidentID = resident.ResidentId;
+                    viewModel.LastName = resident.LastName;
+                    viewModel.FirstName = resident.FirstName;
+                    viewModel.MiddleName = resident.MiddleName;
+                    viewModel.Sex = resident.Sex;
+                    viewModel.Birthdate = resident.Birthday;
+
+                    if (resident.ResidentsLocations.FirstOrDefault() != null)
+                    {
+                        viewModel.Address = resident.ResidentsLocations.FirstOrDefault().HouseHoldAddress.Address;
+                        viewModel.SiteID = resident.ResidentsLocations.FirstOrDefault().HouseHoldAddress.SiteId;
+                    }         
+
+                    return PartialView("_AddUpdateResident", viewModel);
+                }
+
+            }
+            catch (Exception e)
+            {
+                TempData["alert-type"] = "alert-danger";
+                TempData["alert-header"] = "Error";
+                TempData["alert-msg"] = "Something went wrong, please try again later." + e.ToString();
+                return View("Index");
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult AddUpdateResident(RegisterResidentViewModel model)
+        {
+            try
+            {
+                // Add
+                if (model.ResidentID == null)
+                {
+                    return Content("Add");
+                }
+                else // Update
+                {
+                    return Content("Update" + model.Birthdate);
+                }
+
+            }
+            catch (Exception e)
+            {
+                TempData["alert-type"] = "alert-danger";
+                TempData["alert-header"] = "Error";
+                TempData["alert-msg"] = "Something went wrong, please try again later." + e.ToString();
+                return View("Index");
+            }
         }
     }
 }
---
+++ Barangay Management Information System/Barangay Management Information System/Models/RegisterResidentViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace Barangay_Management_Information_System.Models
+{
+    public class RegisterResidentViewModel
+    {
+
+        [Required]
+        public string ResidentID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Sex")]
+        public string Sex { get; set; }
+
+        [Required]
+        [Display(Name = "Birthdate")]
+        [DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birthdate { get; set; }
+
+        [Required]
+        [Display(Name = "Site Location")]
+        public string SiteID { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+    }
+}
