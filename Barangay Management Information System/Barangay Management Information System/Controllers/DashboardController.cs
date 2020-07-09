5906b2886276d4c451014b2b15e400b3e6d6831a
gn-grc <genejogarcia.gg@gmail.com>
2020-07-09 19:27:33 +0800

    dashboard view-number counts, and first line chart

--- Barangay Management Information System/Barangay Management Information System/Controllers/DashboardController.cs
+++ Barangay Management Information System/Barangay Management Information System/Controllers/DashboardController.cs
@@ -1,6 +1,9 @@
 ﻿using Barangay_Management_Information_System.Classess;
+using Barangay_Management_Information_System.Models;
 using Barangay_Management_Information_System.Models.Entity;
+using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +23,36 @@ namespace Barangay_Management_Information_System.Controllers
         {
             TempData["user-profile-photo"] = UserHelper.GetDisplayPicture(User.Identity.GetUserId(), entities);
 
-            return View();
+            DashboardViewModel dbViewModel = new DashboardViewModel();
+
+            int seniorYear = DateTime.Now.Year - 60;
+
+            DateTime senior = new DateTime(seniorYear, DateTime.Now.Month, DateTime.Now.Day);
+
+            dbViewModel.Residents = entities.ResidentsInformations.Count();
+            dbViewModel.SeniorCitizens = entities.ResidentsInformations.Where(m => m.Birthday <= senior).Count();
+            dbViewModel.Deceased = entities.ResidentsInformations.Where(m => m.Deceaseds.FirstOrDefault() != null).Count();
+            dbViewModel.Voters = entities.ResidentsInformations.Where(m => m.Voters.FirstOrDefault() != null).Count();
+
+            return View(dbViewModel);
+        }
+
+        [Authorize]
+        public ContentResult GetDateResidentRecorded()
+        {
+            List<DashboardViewModel> dbvm = new List<DashboardViewModel>();
+
+            List<DateTime> dates = entities.ResidentsInformations.GroupBy(m => m.DateRecorded).Select(m=>m.OrderBy(n=>n.DateRecorded).FirstOrDefault().DateRecorded.Value).ToList();
+
+            foreach (var date in dates)
+            {
+                dbvm.Add( new DashboardViewModel() {
+                    DateCitizenRecord = date.ToString("MMM dd, yyyy"),
+                    DateCitizenRecordCount = entities.ResidentsInformations.Where(m => m.DateRecorded == date).Count()
+                });
+            }
+
+            return Content(JsonConvert.SerializeObject(dbvm), "application/json");
         }
     }
 }
--- Barangay Management Information System/Barangay Management Information System/Models/DashboardViewModel.cs
+++ Barangay Management Information System/Barangay Management Information System/Models/DashboardViewModel.cs
@@ -7,5 +7,15 @@ namespace Barangay_Management_Information_System.Models
 {
     public class DashboardViewModel
     {
+
+        public int Residents { get; set; }
+        public int SeniorCitizens { get; set; }
+        public int Deceased { get; set; }
+        public int Voters { get; set; }
+
+
+        public string DateCitizenRecord { get; set; }
+        public int DateCitizenRecordCount { get; set; }
+
     }
 }
