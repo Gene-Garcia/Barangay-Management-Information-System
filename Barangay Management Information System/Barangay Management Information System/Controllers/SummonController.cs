64e48d09a18650df5c367bafdc0f2e1b71a094a3
gn-grc <genejogarcia.gg@gmail.com>
2020-07-12 09:37:41 +0800

    new controller

---
+++ Barangay Management Information System/Barangay Management Information System/Controllers/SummonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Barangay_Management_Information_System.Controllers
+{
+    public class SummonController : Controller
+    {
+        // GET: Summon
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Barangay Management Information System/Barangay Management Information System/Models/SummonViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Barangay_Management_Information_System.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Barangay_Management_Information_System.Models
+{
+    public class SummonViewModel
+    {
+
+        public Summon Summon { get; set; }
+        public List<string> AssociatedResidentID { get; set; }
+
+    }
+}
